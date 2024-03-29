904f5df2c5ac8954ebd9cd68072306e401dd95eb
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2024-03-29 19:59:02 +0000

    Deploying to gh-pages from @ Vladlen8432/goit-react-hw-05-movies@0c44320d5d99d3ba84829220513ef1798e623c9e 🚀

--- static/js/602.35697b6b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[602],{602:function(e,s,t){t.r(s);var i=t(689),c=t(947),n=t(184);s.default=function(){return(0,n.jsxs)("div",{children:[(0,n.jsx)(c.Z,{}),(0,n.jsx)(i.j3,{})]})}}}]);
-//# sourceMappingURL=602.35697b6b.chunk.js.map
